fcd4d718cb1036e1bca6503aaac016f649da6681
“deep150499” <deepnanavati@icloud.com>
2024-06-18 15:59:31 -0400

    footer section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import ShopCategory from './Pages/ShopCategory';
 import LoginSignup from './Pages/LoginSignup';
 import Cart from './Pages/Cart';
 import Product from './Pages/Product';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
             <Route path='/cart' element={<Cart/>}/>
             <Route path='/login' element={<LoginSignup/>}/>
           </Routes>
+        <Footer/>
     </BrowserRouter>
   );
 }
