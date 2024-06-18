ac3513cb30c646b1378f6212263771f0e1104f2e
“deep150499” <deepnanavati@icloud.com>
2024-06-18 13:54:45 -0400

    initial setup, nav, hero sec

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import LoginSignup from './Pages/LoginSignup';
+import Cart from './Pages/Cart';
+import Product from './Pages/Product';
+
+function App() {
+  return (
+    <BrowserRouter>
+        <Navbar/>
+          <Routes>
+            <Route path='/' element={<Shop/>}/>
+            <Route path='/mens' element={<ShopCategory category='mens'/>}/>
+            <Route path='/womens' element={<ShopCategory category='womens'/>}/>
+            <Route path='/kids' element={<ShopCategory category='kids'/>}/>
+            <Route path='/product' element={<Product/>}>
+              <Route path=':productId' element={<Product/>}/>
+            </Route>
+            <Route path='/cart' element={<Cart/>}/>
+            <Route path='/login' element={<LoginSignup/>}/>
+          </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
