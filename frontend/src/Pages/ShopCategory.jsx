94df64492a9c00d46eb7c075293745d71930fb2e
“deep150499” <deepnanavati@icloud.com>
2024-06-18 16:49:06 -0400

    context setup and added banner

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+
+  const {all_products} = useContext(ShopContext)
+
   return (
-    <div>ShopCategory</div>
+    <div className='shop-category'>
+      <img src={props.banner} alt="" />
+    </div>
   )
 }
 
