e1e4969ece17c01e0024ec3f2a782f0fe50a33f3
“deep150499” <deepnanavati@icloud.com>
2024-06-19 17:07:58 -0400

    login signup page

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom'
 
 const Product = () => {
+
+  const {all_product} = useContext(ShopContext)
+  const {productId} = useParams();
+  const product = all_product.find((e) => e.id === Number(productId))
+
   return (
-    <div className='product'>Product</div>
+    <div className='product'></div>
   )
 }
 
