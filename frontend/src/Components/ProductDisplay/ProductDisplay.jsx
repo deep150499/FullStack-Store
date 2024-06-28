4c7db36da13fbf49747b010f1721224b35d8c060
“deep150499” <deepnanavati@icloud.com>
2024-06-28 09:11:43 -0400

    product-display and listProduct updated

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,57 +1,70 @@
-import React, { useContext } from 'react'
-import './ProductDisplay.css'
-import star_icon from '../Assets/star_icon.png'
-import star_dull_icon from '../Assets/star_dull_icon.png'
-import { ShopContext } from '../../Context/ShopContext'
+import React, { useContext } from "react";
+import "./ProductDisplay.css";
+import star_icon from "../Assets/star_icon.png";
+import star_dull_icon from "../Assets/star_dull_icon.png";
+import { ShopContext } from "../../Context/ShopContext";
+import { backend_url, currency } from "../../App";
 
 const ProductDisplay = (props) => {
-
-    const {product} = props;
-    const {addToCart} = useContext(ShopContext)
+  const { product } = props;
+  const { addToCart } = useContext(ShopContext);
 
   return (
-    <div className='productdisplay'>
-        <div className='productdisplay-left'>
-            <div className='productdisplay-img-list'>
-                <img src={product.image} alt="" />
-                <img src={product.image} alt="" />
-                <img src={product.image} alt="" />
-            </div>
-            <div className="productdisplay-img">
-                <img className='productdisplay-main-img' src={product.image} alt="" />
-            </div>
+    <div className="productdisplay">
+      <div className="productdisplay-left">
+        <div className="productdisplay-img-list">
+          <img src={backend_url + product.image} alt="img" />
+          <img src={backend_url + product.image} alt="img" />
+          <img src={backend_url + product.image} alt="img" />
+          <img src={backend_url + product.image} alt="img" />
         </div>
-        <div className='productdisplay-right'>
-                <h1>{product.name}</h1>
-                <div className='productdisplay-right-star'>
-                    <img src={star_icon} alt="" />
-                    <img src={star_icon} alt="" />
-                    <img src={star_icon} alt="" />
-                    <img src={star_icon} alt="" />
-                    <img src={star_dull_icon} alt="" />
-                    <p>(122)</p>
-                </div>
-                <div className='productdisplay-right-prices'>
-                    <div className='productdisplay-right-price-old'>${product.old_price}</div>
-                    <div className='productdisplay-right-price-new'>${product.new_price}</div>
-                </div>
-                <div className="productdisplay-right-description">
-                Embrace timeless style with our Vintage Graphic Tee, featuring a classic retro design that adds a nostalgic touch to your wardrobe. Crafted from ultra-soft, breathable cotton, this tee ensures all-day comfort and pairs effortlessly with jeans or shorts for a relaxed, stylish look.
-                </div>
-                <div className='productdisplay-right-size'>
-                    <h1>Size</h1>
-                    <div className='productdisplay-right-size'>
-                        <div>S</div>
-                        <div>M</div>
-                        <div>L</div>
-                        <div>XL</div>
-                        <div>XXL</div>
-                    </div>
-                </div>
-                <button onClick={() => addToCart(product.id)}>ADD TO CART</button>
+        <div className="productdisplay-img">
+          <img
+            className="productdisplay-main-img"
+            src={backend_url + product.image}
+            alt="img"
+          />
         </div>
+      </div>
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-star">
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_dull_icon} alt="" />
+          <p>(122)</p>
+        </div>
+        <div className="productdisplay-right-prices">
+          <div className="productdisplay-right-price-old">
+            ${product.old_price}
+          </div>
+          <div className="productdisplay-right-price-new">
+            ${product.new_price}
+          </div>
+        </div>
+        <div className="productdisplay-right-description">
+          Embrace timeless style with our Vintage Graphic Tee, featuring a
+          classic retro design that adds a nostalgic touch to your wardrobe.
+          Crafted from ultra-soft, breathable cotton, this tee ensures all-day
+          comfort and pairs effortlessly with jeans or shorts for a relaxed,
+          stylish look.
+        </div>
+        <div className="productdisplay-right-size">
+          <h1>Select Size</h1>
+          <div className="productdisplay-right-sizes">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+        </div>
+        <button onClick={() => addToCart(product.id)}>ADD TO CART</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDisplay
+export default ProductDisplay;
